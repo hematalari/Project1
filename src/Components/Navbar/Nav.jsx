c3c28b88ed643ce12b409a7cfb0564575f9ed871
hematalari <hema.talari08@gmail.com>
2024-07-01 13:49:16 +0530

    image path change

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -1,6 +1,6 @@
 import "./Nav.css";
 import React from 'react';
-import logo from "../../assets/Easy crets images/logo.png";
+import logo from "../../images/logo.png";
 import {Link} from 'react-router-dom';
 const Nav = () => {
   return (
