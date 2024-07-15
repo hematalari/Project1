f631495247813f0530d617a3b233b4088a61f32a
hematalari <hema.talari08@gmail.com>
2024-07-15 15:14:13 +0530

    social media path

--- src/Components/Footer1/Footer1.jsx
+++ src/Components/Footer1/Footer1.jsx
@@ -9,16 +9,16 @@ const Footer1 = () => {
       <FaCopyright style={{color:'white', fontSize:'larger'}}/>
         <p>2024. All Rights Reserved. Easy Crets</p>
         <div className="copyright1">
-            <a href='https://www.facebook.com/'>
+            <a href='https://www.facebook.com/share/NkDcpArbiFSnPym2/?mibextid=qi2Omg'>
       <FaFacebook />
       </a>
-      <a href='https://www.instagram.com/'>
+      <a href='https://www.instagram.com/easycerts?igsh=MWEwMnlhMmp1bHVpbw=='>
       <FaInstagram />
       </a>
       <a href='https://twitter.com/i/flow/login'>
       <FaTwitter />
       </a>
-      <a href='https://www.youtube.com/'>
+      <a href='https://www.youtube.com/@EasyCerts-t3r'>
       <FaYoutube />
       </a>
       </div>
